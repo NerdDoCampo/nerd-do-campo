70906937bffa7d3203fa387b6aa7cd123264e9f3
NerdDoCampo <anderson_volnei@hotmail.com>
2026-06-18 21:37:58 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Cache versionado: muda a cada release, forçando atualização.
-const CACHE_NAME = 'nerd-do-campo-1.19.8';
+const CACHE_NAME = 'nerd-do-campo-1.20.0';
 const STATIC_ASSETS = ['/manifest.json'];
 
 self.addEventListener('install', event => {
--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import { default as AdminApp } from './Admin';
 import SuperApp from './Super';
 import RecuperarSenha, { ehLinkRecuperacao } from './RecuperarSenha';
 import Confirmar, { ehLinkConfirmacao } from './Confirmar';
+import Conheca from './Conheca';
 
 const path = window.location.pathname;
 const isAdmin = path.startsWith('/admin');
 const isSuper = path.startsWith('/super');
+const isConheca = path.startsWith('/conheca');
 const isRecuperacao = ehLinkRecuperacao();
 const isConfirmacao = ehLinkConfirmacao();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {isConfirmacao ? <Confirmar /> : isRecuperacao ? <RecuperarSenha /> : isSuper ? <SuperApp /> : isAdmin ? <AdminApp /> : <App />}
+    {isConfirmacao ? <Confirmar /> : isRecuperacao ? <RecuperarSenha /> : isSuper ? <SuperApp /> : isAdmin ? <AdminApp /> : isConheca ? <Conheca /> : <App />}
   </React.StrictMode>
 );
