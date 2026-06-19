e7f3d43137a207a5e03cf66832bdc78eb9cd897b
NerdDoCampo <anderson_volnei@hotmail.com>
2026-06-19 10:24:08 -0300

    Add files via upload

--- src/Super.jsx
+++ src/Super.jsx
@@ -2738,7 +2738,7 @@ function CrudTipoTime({ show }) {
 export default function SuperApp() {
   const [session, setSession] = useState(SESSION_TOKEN ? {access_token: SESSION_TOKEN} : null);
   const [sessaoExpirou, setSessaoExpirou] = useState(false);
-  const APP_VERSION = process.env.REACT_APP_VERSION || "1.20.3";
+  const APP_VERSION = process.env.REACT_APP_VERSION || "1.20.5";
 
   useEffect(() => {
     const handler = () => { setSessaoExpirou(true); setSession(null); };
