67fda00390227bb01af369fce915dd33ca5d83cd
NerdDoCampo <anderson_volnei@hotmail.com>
2026-07-17 11:05:20 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Cache versionado: muda a cada release, forçando atualização.
-const CACHE_NAME = 'nerd-do-campo-1.30.0';
+const CACHE_NAME = 'nerd-do-campo-1.30.1';
 const STATIC_ASSETS = ['/manifest.json'];
 
 self.addEventListener('install', event => {
